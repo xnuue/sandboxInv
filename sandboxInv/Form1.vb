0c933d63b1b86c5804eb1d7afc618f57d3de95f9
xnuue <syzysyzy@protonmail.com>
2023-10-30 19:16:22 +0800

    sxex

--- sandboxInv/Form1.vb
+++ sandboxInv/Form1.vb
@@ -1,5 +1,11 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("hi")
+        Dim url As String = "https://www.youtube.com/watch?v=a5VwSeCOMQU&t=7s"
+        Dim url1 As String = "https://www.facebook.com/rafaeljetrocipriano"
+        Dim url2 As String = "https://www.tiktok.com/@creamsonjet"
+        MessageBox.Show("subscribe to my youtube channel: " + "CREAMSON")
+        Process.Start(url)
+        Process.Start(url1)
+        Process.Start(url2)
     End Sub
 End Class
--- sandboxInv/My Project/Application.Designer.vb
+++ sandboxInv/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.sandboxInv.main
+            Me.MainForm = Global.sandboxInv.Form1
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
